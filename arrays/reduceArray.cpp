6debce19459d33cf974e1130ffae2440f1e68822
darthchild <ekagranigam@gmail.com>
2023-12-16 01:34:37 +0530

    .

---
+++ arrays/reduceArray.cpp
@@ -0,0 +1,19 @@
+/*
+REDUCE THE GIVEN ARRAY 
+
+Replace all the elements of the array with the index
+it would have if the array was sorted 
+
+{ 15, 10, 25, 20 }  ->  { 1, 0, 3 , 2 }
+
+*/
+
+
+#include <bits/stdc++.h>
+using namespace std; 
+
+
+int main(){
+
+
+}
