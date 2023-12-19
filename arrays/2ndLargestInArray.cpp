1376184b4f228751be969d5e1a75b9f22e6fcaca
darthchild <ekagranigam@gmail.com>
2023-12-20 02:19:26 +0530

    1

--- arrays/2ndLargestInArray.cpp
+++ arrays/2ndLargestInArray.cpp
@@ -1,39 +1,30 @@
-// FIND THE SECOND LARGEST ELEMENT IN THE ARRAY 
 
-#include <bits/stdc++.h> 
+#include <bits/stdc++.h>
+
 using namespace std;
 
-int seclargestElement(vector<int> &arr, int n) {
-    // Write your code here.
+void findSecMax(vector<int> arr){
+	int max = arr[0];
+	int secMax = 0;
 
-    int max;
+	// finds largest element
+	for(int i : arr){
+		if(i > max)
+			max = i;
+	}
 
-    for(int i=0; i<n; i++){
-       max = arr[i];
-       for(int j=0; j<n; j++){
-           if(arr[j] > max){
-               max = arr[j];
-           }
-       } 
-    }
-
-    int secMax = 0;
-    for (int i = 0; i < n; i++)
-    {
-        int diff = max - arr[i];
-
-        if( diff < max - secMax && diff != 0){
-            secMax = arr[i];
-        }
-    }
-    
-    cout << secMax<< "\n";
-    cout << max<< "\n";
-    
+	// finds 2nd largest element
+	for(int i : arr){
+		if(max-i < max-secMax && i != max)
+			secMax = i;
+	}
+	cout << max << " " << secMax << endl;
 }
 
-int main(){
-    vector<int> arr = {3,9,4,12,8,10,6,5};
-    largestElement(arr,arr.size());
 
+int main(){
+
+	vector<int> arr = {-1,0,4,-3};
+	findSecMax(arr);
+	
 }
---
+++ arrays/KthLargest.cpp
@@ -0,0 +1,46 @@
+/*
+215. Kth Largest Element in an Array
+
+Given an integer array nums and an integer k, return the kth largest 
+element in the array.
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int quickSelect(vector<int>& nums, int k) {
+    int pivot = nums[rand() % nums.size()];
+    
+    vector<int> left;
+    vector<int> mid;
+    vector<int> right;
+    
+    for (int num: nums) {
+        if (num > pivot) {
+            left.push_back(num);
+        } else if (num < pivot) {
+            right.push_back(num);
+        } else {
+            mid.push_back(num);
+        }
+    }
+    
+    if (k <= left.size()) {
+        return quickSelect(left, k);
+    }
+    
+    if (k > left.size() + mid.size() ) {
+        return quickSelect(right, k - left.size() - mid.size());
+    }
+    
+    return pivot;
+    
+}
+
+void main(){
+
+    vector<int> arr = {3,5,2,1,6,4};
+    k = 2;
+
+    quickSelect(arr,k);
+}
