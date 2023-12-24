9c7f25ecef2c02b8623198ac3055d29e49e26119
darthchild <ekagranigam@gmail.com>
2023-12-24 17:56:08 +0530

    4

--- arrays/findIntersection.cpp
+++ arrays/findIntersection.cpp
@@ -1,3 +1,23 @@
+/*
+349. Intersection of Two arrays
+
+Given two integer arrays nums1 and nums2, return 
+an array of their intersection. Each element in the result 
+must be unique and you may return the result in any order.
+
+ 
+Example 1:
+
+Input: nums1 = [1,2,2,1], nums2 = [2,2]
+Output: [2]
+
+Example 2:
+
+Input: nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+Output: [9,4]
+([4,9] is also accepted.)
+*/
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -22,7 +42,7 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
             i++;
             j++;
             
-            // Skip duplicates
+            // Double check for duplicates
             while (i < n1 && nums1[i] == nums1[i - 1]) {
                 i++;
             }  
@@ -37,8 +57,8 @@ vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
 
 int main(){
 
-    vector<int> nums1 = {4,9,5};
-    vector<int> nums2 = {9,4,9,8,4};
+    vector<int> nums1 = {6,1,2,3,1,2};
+    vector<int> nums2 = {2,2,1,6};
 
     vector<int> ans = intersection(nums1,nums2);
 
