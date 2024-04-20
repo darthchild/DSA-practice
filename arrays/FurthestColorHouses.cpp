9a1eb77edf1feeb8605afcf148f9e0d53aacdf11
darthchild <ekagranigam@gmail.com>
2024-04-21 01:17:48 +0530

    13

--- arrays/FurthestColorHouses.cpp
+++ arrays/FurthestColorHouses.cpp
@@ -1,18 +1,18 @@
 /*
-2078. Two Furthest Houses With Different Colors
+2078. Two Furthest Houses With Different a
 
 There are n houses evenly lined up on the street, and each house is painted.
-You are given a 0-indexed integer array colors of length n, where colors[i]
+You are given a 0-indexed integer array a of length n, where a[i]
 represents the color of the ith house.
 
-Return the maximum distance between two houses with different colors.
+Return the maximum distance between two houses with different a.
 
                  '     '
-Input: colors = [1,1,1,6,1,1,1]
+Input: a = [1,1,1,6,1,1,1]
 Output: 3
 
                  '       '
-Input: colors = [1,8,3,8,3]
+Input: a = [1,8,3,8,3]
 Output: 4
 */
 
@@ -46,21 +46,20 @@ int maxDistance(vector<int>& a) {
 // One pass O(n) solution 
 int maxDistance1(vector<int>& a) {
 
-    int start = 0;
-    int end = a.size() - 1;
-    int hold = start, ret = 0;
-    
-    while(hold < a.size()){
+    int n = a.size();
+    int max1 = 0,max2 = 0;
+    int paint1 = a[0], paint2 = a[n-1];
+    int start1 = 0, start2 = n-1;
 
-        if(a[start] != a[hold]){
-            int max1 = max(ret, hold - start);
-            int max2 = max(ret, end - hold);
-            ret = max(max1,max2);    
-        }
-        
-        hold++;
+    for(int i=1; i<n; i++){
+        if(a[i]!=paint1) 
+            max1 = max(max1,abs(i-start1));
+
+        if(a[n-i-1]!=paint2) 
+        max2 = max(max2,abs(n-i-1-start2));
     }
-    return ret;
+    return max(max1,max2);
+
 }
 
 int main(){
