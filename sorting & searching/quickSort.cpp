ea60e912ed78775e292b07e61d090e7733f280b7
darthchild <ekagranigam@gmail.com>
2024-05-30 01:28:20 +0530

    15

--- sorting & searching/quickSort.cpp
+++ sorting & searching/quickSort.cpp
@@ -1,47 +1,39 @@
-// Quick Sort 
+// Quick Sort algorithm
 
 #include <bits/stdc++.h>
 using namespace std;
 
-void swap(int &a, int &b){
-    int temp = a;
-    a = b;
-    b = temp;
-}
-
 int partition(vector<int> &arr, int start, int end){
 
-    int pivot = arr[start];
-    int i = start;
-    int j = end;
+    int pvt = arr[start];
+    int i = start+1, j = end;
 
     while(i < j){
-        while(arr[i] <= pivot)
+        while(arr[i] < pvt && i < arr.size())
             i++;
-        while(arr[j] > pivot)
+        while(arr[j] >= pvt)
             j--;
         if(i < j)
             swap(arr[i],arr[j]);
     }
     //when j has crossed i 
-    swap(arr[start],arr[j]); // now pvt element is in its sorted position
+    swap(arr[start],arr[j]); // now pivot element is in its sorted position
     return j; //returns pivot element's index
 }
 
 void quickSort(vector<int> &arr, int start, int end){
     if(start < end){
-        int pivot = partition(arr,start,end);
-        quickSort(arr,start,pivot-1);
-        quickSort(arr,pivot+1,end);
+        int pvt = partition(arr,start,end);
+        quickSort(arr,start,pvt-1);
+        quickSort(arr,pvt+1,end);
     }
 }
 
 int main(){
 
-    vector<int> arr = {6,5,9,1,4,8};
-    quickSort(arr,0,5);
+    vector<int> arr = {2,0,1};
+    quickSort(arr,0,arr.size()-1);
 
-    for(int i: arr){
+    for(int i: arr)
         cout << i << " ";
-    }
 }
