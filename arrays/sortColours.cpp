56c8dd224fbb3a67ee1f9d68baabcbd65ae54007
darthchild <ekagranigam@gmail.com>
2024-06-02 01:44:24 +0530

    15

--- arrays/sortColours.cpp
+++ arrays/sortColours.cpp
@@ -23,15 +23,19 @@ using namespace std;
 void sortColors(vector<int>& nums) {
     int low = 0, mid = 0, high = nums.size()-1;
     while(mid <= high){
-        if(nums[mid] == 0){
+
+        if(nums[mid] == 0)
+        {
             swap(nums[low], nums[mid]);
             low++;
             mid++;
         }
-        else if(nums[mid] == 1){
+        else if(nums[mid] == 1)
+        {
             mid++;
         }
-        else{
+        else
+        {
             swap(nums[mid], nums[high]);
             high--;
         }
